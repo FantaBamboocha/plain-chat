83dfa61e9870c6ce46c424e0c05766082912829b
FantaBamboocha <moomentomoori@gmail.com>
2024-04-29 18:53:33 +0300

    configure socket

--- server.cjs
+++ server.cjs
@@ -1,23 +1,24 @@
 const express = require("express");
-const soket = require("socket.io");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
 
 const app = express();
-const server = require("http").Server(app);
-const io = soket(server);
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://127.0.0.1:5173", // Указываем разрешенный источник
+    methods: ["GET", "POST"], // Указываем разрешенные методы
+  },
+});
 
-const dataBase = new Map();
-
-app.get("/wow", (req, res) => {
-  res.json(dataBase);
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
 });
 
 io.on("connection", (socket) => {
-  console.log("user connected", socket);
+  console.log("a user connected", socket.id);
 });
 
-server.listen(3000, (error) => {
-  if (error) {
-    throw new Error(error);
-  }
-  console.log("PLAIN-CHAT-APP listening on port 3000!");
+server.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
 });
